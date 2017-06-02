453f292cad2f0934da51116612aad556d881610c
Bryan's Air <BryansAppleAir@BryBook.local>
2017-06-02 10:02:41 -0600

    test add animation code unworking

--- Alien Adventure/SpecialBadge.swift
+++ Alien Adventure/SpecialBadge.swift
@@ -22,7 +22,40 @@ class SpecialBadge: Badge {
     }
     
     enum BadgeAnimation: Int {
-        case growAndShrink, rotate, shake
+        case growAndShrink = 0, rotate, shake
     }
     
+    func randomAnimation() -> BadgeAnimation {
+        let randomNumber = arc4random_uniform(3)
+        return BadgeAnimation(rawValue: Int(randomNumber))!
+    }
+    
+    var animation = randomAnimation()
+    
+    switch animation {
+    case growAndShrink:
+        let action1 = SKAction.scale(to: 0.8, duration: 1.0)
+        let action2 = SKAction.scale(to: 1.1, duration: 1.0)
+        let sequencedAction = SKAction.sequence([action1, action2])
+        run(SKAction.repeatForever(sequencedAction))
+    case rotate:
+        let action = SKAction.rotate(byAngle: CGFloat(-M_PI), duration: 1.5)
+        run(SKAction.repeatForever(action))
+    case shake:
+        let x: Float = 10
+        let y: Float = 6
+        let numberOfTimes = 2.0 / 0.04
+        var actionsArray = [SKAction]()
+        
+        for _ in 1...Int(numberOfTimes) {
+        let dX = Float(arc4random_uniform(UInt32(x))) - x / 2;
+        let dY = Float(arc4random_uniform(UInt32(y))) - y / 2;
+        let shakeAction = SKAction.moveBy(x: CGFloat(dX), y: CGFloat(dY), duration: 0.02);
+        actionsArray.append(shakeAction);
+        actionsArray.append(shakeAction.reversed());
+        }
+        
+        let sequencedAction = SKAction.sequence(actionsArray)
+        run(SKAction.repeatForever(sequencedAction))
+    }
 }
